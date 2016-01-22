f48827ad1385e382c590592bab5697c3b8a81165
Josh Quintana <josh_d_quintana@homedepot.com>
2016-01-22 10:39:18 -0500

    Initial tests pass

---
+++ src/main/java/dbbroker/client/CredentialFactory.java
@@ -0,0 +1,13 @@
+package dbbroker.client;
+
+public class CredentialFactory {
+
+    /*
+
+     */
+    public static String getPassword ( String appId, String spaceId,
+                                       String dbUsername ) {
+        return "password";
+    }
+
+}
